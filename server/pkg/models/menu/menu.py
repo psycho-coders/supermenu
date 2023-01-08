b17d06f18b95346efdc1b42a9969b2e5caeef3aa
Zhan <33184437+road2global@users.noreply.github.com>
2023-01-08 17:52:10 +0600

    feat(NN-1): update get list (#1)
    Added list handler

--- server/pkg/models/menu/menu.py
+++ server/pkg/models/menu/menu.py
@@ -2,17 +2,19 @@ from pkg.models import db
 
 
 class Menu:
-    def __init__(self, name, params):
-        self.name = name
-        self.params = params
+    def __init__(self, id, data):
+        self.id = id
+        self.data = data
 
-    def get():
-        pass
+    def get(self):
+        doc = db.collection(u'menu').where(u'id', u'==', self.id)
+        if doc.exists:    
+            self.data = doc.get().to_dict()
 
     def create(self):
-        doc_ref = db.collection(u'menu').document(f'{self.name}')
+        doc_ref = db.collection(u'menu').document(f'{self.id}')
         doc_ref.set({
-            u'data': self.params,
+            u'data': self.data,
         })
 
     def delete():
--- server/web/v1/menu/list.py
+++ server/web/v1/menu/list.py
@@ -1,6 +1,7 @@
 from . import menu_handler
-
+from pkg.models.menu.menu import Menu
 
 @menu_handler.route("/list")
-def list():
-    return "list"
+def list(): 
+    m=Menu("1", "<body> </body").get()
+    return m.data
