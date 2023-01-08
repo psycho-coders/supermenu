cf3786fca189f04b3d6ca67e480dca227239e244
Zhan <33184437+road2global@users.noreply.github.com>
2023-01-08 16:57:11 +0600

    Update menu.py

--- server/pkg/models/menu/menu.py
+++ server/pkg/models/menu/menu.py
@@ -6,8 +6,11 @@ class Menu:
         self.name = name
         self.params = params
 
-    def get():
-        pass
+    def get(self, name):
+        doc = db.collection(u'menu').document(u'{name}').format(name=name)
+        if doc.exists:    
+            return (f'{name} ==> {doc.get().to_dict()}'), 200
+        return "Data not found", 404
 
     def create(self):
         doc_ref = db.collection(u'menu').document(f'{self.name}')
