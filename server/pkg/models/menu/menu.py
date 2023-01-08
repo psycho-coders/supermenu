47308af5156bd3b134f791ecfbe3cc5f285be855
Zhan <33184437+road2global@users.noreply.github.com>
2023-01-08 17:50:55 +0600

    fix: bug fix

--- server/pkg/models/menu/menu.py
+++ server/pkg/models/menu/menu.py
@@ -10,7 +10,6 @@ class Menu:
         doc = db.collection(u'menu').where(u'id', u'==', self.id)
         if doc.exists:    
             self.data = doc.get().to_dict()
-        else: self.data = dict()
 
     def create(self):
         doc_ref = db.collection(u'menu').document(f'{self.id}')
