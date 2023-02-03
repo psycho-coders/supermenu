2e9cfffc062f4f7ae912872912300fb4292fe8c2
personal <road2train@gmail.com>
2023-02-03 22:45:33 +0600

    menu creation update

--- server/web/v1/menu/create.py
+++ server/web/v1/menu/create.py
@@ -1,8 +1,13 @@
 from . import menu_handler
 from pkg.models.menu.menu import Menu
+from flask import Flask, json, request
 
-
-@menu_handler.route("/create")
+@menu_handler.route("/create", methods=['POST'])
 def create():
-    Menu("1", "first", "<body> </body").create()
-    return "created"
+    menu_details = request.get_json()
+    try: 
+        Menu(menu_details['creator'], menu_details['name'], menu_details['sections']).create()
+        return "created"
+    except Exception as err:
+        return "error"
+
