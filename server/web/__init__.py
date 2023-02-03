69a5b140b69e404abda335b592da85564c5c6121
Zhan <33184437+road2global@users.noreply.github.com>
2023-02-03 23:31:53 +0600

    Update __init__.py

--- server/web/__init__.py
+++ server/web/__init__.py
@@ -1,9 +1,9 @@
 from flask import Flask
-
+from flask_cors import CORS
 
 def create_app():
     app = Flask("server")
-
+    CORS(app)
     from .v1 import v1_app
     app.register_blueprint(v1_app, url_prefix="/v1")
 
